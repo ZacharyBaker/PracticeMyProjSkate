b56f8efc5a3025b0d6aada90817d8e7f1166cc4f
Zach Baker <zach.r.baker1@gmail.com>
2015-11-07 12:59:20 -0700

    updated the coment in parkCtrl

--- controllers/parkCtrl.js
+++ controllers/parkCtrl.js
@@ -6,5 +6,7 @@ angular.module('skateApp')
 	
 	
 	// play with the service now!!! with the $scope.park
+	// in order to get the data for the park that was clicked on because the change in url/controllers 
+	// flushed all the data so you need to get it in order to display it
 	
 }]);
