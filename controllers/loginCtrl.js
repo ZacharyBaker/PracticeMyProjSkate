3da34813bec5a3bd764fc270308939e60aa6023c
Zach Baker <zach.r.baker1@gmail.com>
2015-11-06 17:01:55 -0700

    the button on the first page will now log the select box

--- controllers/loginCtrl.js
+++ controllers/loginCtrl.js
@@ -2,12 +2,16 @@ angular.module('skateApp')
 
 .controller('loginCtrl', ['$scope', function($scope){
 
-
+	
 
 	$scope.dude = function(){
 		console.log('heckka pushed');
 	};
 	
-	
+	$scope.changeState = function(mystate){
+		//this needs to change the state!
+		//u$state.go('')
+		console.log(mystate);
+	}
 	
 }])
